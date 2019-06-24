1ec866d1e0551adef58be7c6a5769b741bc76032
Alisson Kendi Kohatsu <kohatsukendi@gmail.com>
2019-06-24 11:31:52 -0300

    Projeto criado

---
+++ src/main/java/com/apporacao/AppOraçãoApplication.java
@@ -0,0 +1,13 @@
+package com.apporacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOraçãoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOraçãoApplication.class, args);
+	}
+
+}
